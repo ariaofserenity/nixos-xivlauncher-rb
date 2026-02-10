43d532ada3e2f5b6cd2a07b5c7fb3ba43382020e
Aria <ariaserenityvt@gmail.com>
2026-02-09 23:12:02 -0500

    Upgrade .NET SDK and runtime to version 9.0
    Updated .NET SDK and runtime versions to 9.0.

--- xivlauncher-rb/default.nix
+++ xivlauncher-rb/default.nix
@@ -51,8 +51,8 @@ buildDotnetModule rec {
   nugetDeps = ./deps.json; # File generated with `nix-build -A xivlauncher-rb.passthru.fetch-deps`
 
   # please do not unpin these even if they match the defaults, xivlauncher is sensitive to .NET versions
-  dotnet-sdk = dotnetCorePackages.sdk_8_0;
-  dotnet-runtime = dotnetCorePackages.runtime_8_0;
+  dotnet-sdk = dotnetCorePackages.sdk_9_0;
+  dotnet-runtime = dotnetCorePackages.runtime_9_0;
 
   dotnetFlags = [
     "-p:BuildHash=${tag}"
